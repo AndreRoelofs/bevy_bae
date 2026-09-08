210f0bdba2ac9fc73adde64b0c25694ee024e449
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2026-09-08 10:41:08 +0200

    refactor: migrate to bevy 0.19

--- src/plan/update.rs
+++ src/plan/update.rs
@@ -1,6 +1,6 @@
 //! Contains the [`UpdatePlan`] [`EntityEvent`].
 
-use bevy_ecs::error::{DefaultErrorHandler, HandleError as _};
+use bevy_ecs::error::FallbackErrorHandler;
 use bevy_ecs::system::command::run_system_cached_with;
 use core::marker::PhantomData;
 
@@ -49,7 +49,7 @@ pub struct ReplacePlan {
 pub(crate) fn update_plan(
     update: On<UpdatePlan>,
     mut commands: Commands,
-    error_handler: Option<Res<DefaultErrorHandler>>,
+    error_handler: Option<Res<FallbackErrorHandler>>,
 ) {
     let entity = update.entity;
     let error_handler = error_handler.map(|h| *h).unwrap_or_default();
